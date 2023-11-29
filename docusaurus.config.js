5150baa5e0300113e3d0057439401053f9365e79
admin-shesha <122363505+admin-shesha@users.noreply.github.com>
2023-11-29 06:23:07 +0200

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://docs-shesha.azurewebsites.net/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://github.com/shesha-io/docs.shesha/',
+  baseUrl: 'https://docs-shesha.azurewebsites.net/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
