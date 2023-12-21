ef8823bdf3fe09a2bdf296d75adaa376e89ddbf8
Omolemo Lethuloe <omolemo.lethuloe@shesha.io>
2023-12-21 11:59:55 +0200

    home route redirect

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,61 +1,7 @@
-import clsx from 'clsx';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import Layout from '@theme/Layout';
-import Heading from '@theme/Heading';
-import styles from './index.module.css';
-import React, { useEffect } from 'react';
 
-
-function HomepageHeader() {
-    const { siteConfig } = useDocusaurusContext();
-    return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className="container">
-                <Heading as="h1" className="hero__title">
-                    {siteConfig.title}
-                </Heading>
-                <p className="hero__subtitle">{siteConfig.tagline}</p>
-                <div className={styles.buttons}>
-
-                </div>
-            </div>
-        </header>
-    );
-}
+import React from 'react';
+import { Redirect } from '@docusaurus/router';
 
 export default function Home() {
-    const { siteConfig } = useDocusaurusContext();
-
-    useEffect(() => {
-        (function (m, a, z, e) {
-            var s, t;
-            try {
-                t = m.sessionStorage.getItem('maze-us');
-            } catch (err) { }
-
-            if (!t) {
-                t = new Date().getTime();
-                try {
-                    m.sessionStorage.setItem('maze-us', t);
-                } catch (err) { }
-            }
-
-            s = a.createElement('script');
-            s.src = z + '?apiKey=' + e;
-            s.async = true;
-            a.getElementsByTagName('head')[0].appendChild(s);
-            m.mazeUniversalSnippetApiKey = e;
-        })(window, document, 'https://snippet.maze.co/maze-universal-loader.js', '066a039c-261c-47f1-a5b5-0d59aa7efbad');
-    }, []);
-
-    return (
-        <Layout
-            title={`Hello from ${siteConfig.title}`}
-            description="Description will go into a meta tag in <head />">
-            <HomepageHeader />
-            <main>
-            </main>
-        </Layout>
-    );
+    return <Redirect to="/docs/get-started/Introduction" />;
 }
